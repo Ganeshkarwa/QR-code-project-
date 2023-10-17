bf0025e78cbe43f7c6bf165d7534dae19ee20c3e
Ganesh Karwa <140792447+Ganeshkarwa@users.noreply.github.com>
2023-10-17 11:05:12 +0530

    Add files via upload
    What is a QR Code generator? It is a niche tool that is used to generate different types of QR Codes. Depending on your purpose, you can use our generator to create QR Codes to open a website, view a PDF file, listen to music, watch YouTube videos, store image files, connect to a WiFi network, and much more.

---
+++ qrcode.py
@@ -0,0 +1,11 @@
+import qrcode
+qr = qrcode.QRCode(
+    version=15,
+    box_size=10,
+    border=5
+)
+data = "https://github.com/Ganeshkarwa/Ganeshkarwa"
+qr.add_data(data)
+qr.make(fit=True)
+img = qr.make_image(fill="black", back_colour="white")
+img.save('myqr.jpg')
